f7cb25ac213d35890dbf0925bf9992ac233d5b66
Camillo <eduardo.camillo15@gmail.com>
2024-09-20 23:18:07 -0300

    #1 - Now the database is connected with phpmyAdmin

--- database.js
+++ database.js
@@ -2,18 +2,27 @@ const mysql = require('mysql2');
 
 var sql = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'sysadmin',
     password: 'q1w2e3r4',
-    database: 'teste'
+    database: 'cloud'
 }
 );
 
 
 
-sql.connect(function(error){
-    if(error){
-        console.log("Deu péssimo de novo: " + error);
-        return;
-    }
-    console.log("Deu bom")
-});
+try{
+    sql.connect();
+    console.log("Conexão realizada");
+}catch(e){
+    console.log("Erro: " + e)
+}
+var email = "teste1"
+var senha = "senha"
+
+
+try{
+    console.log("Realizando inserção:")
+    sql.query('INSERT INTO users(email, password) VALUES( ' + '"' + email  + '"' + ', ' + '"' + senha  + '"' + ');')
+}catch(error){
+    console.log("DEU ERRO: " + error)
+}
